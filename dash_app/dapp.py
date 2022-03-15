80428df132591493c204ba106d15e474b41ec5a6
kkimatx <kkimatx@gmail.com>
2022-03-15 16:35:55 -0700

    gunicorn setting

--- dash_app/dapp.py
+++ dash_app/dapp.py
@@ -8,7 +8,7 @@ import pandas as pd
 # initiate app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-server = flask.Flask(__name__)
+# server = flask.Flask(__name__)
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets,
     suppress_callback_exceptions=True)
