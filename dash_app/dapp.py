512f386836c8b707581ca27e5967e8cd27a68cdf
kkimatx <kkimatx@gmail.com>
2022-03-16 12:20:31 -0700

    debug mode on deployment

--- dash_app/dapp.py
+++ dash_app/dapp.py
@@ -12,6 +12,7 @@ server = flask.Flask(__name__)
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets,
     suppress_callback_exceptions=True)
+app.enable_dev_tools(debug=True)
 
 # set up server-side cache.
 
